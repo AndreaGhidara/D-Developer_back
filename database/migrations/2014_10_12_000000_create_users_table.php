d610ce930f04a0b52ada3cad12caac3bae89764c
valeria caramellino <valeriacaramellino@gmail.com>
2023-08-24 10:28:42 +0200

    create user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',50);
+            $table->string('surname',50);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_of_birth');
+            $table->string('address');
+            $table->text('bio')->nullable();
+            $table->text('img_path')->nullable();
+            $table->text('bg_dev')->nullable();
+            $table->text('github_link')->nullable();
+            $table->text('linkedin_link')->nullable();
+            $table->string('vat_number')->unique();
+            $table->text('cv')->nullable();
+            $table->string('phone_number');
+            $table->text('soft_skill')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

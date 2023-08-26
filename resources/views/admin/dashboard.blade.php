3a6a13806c7186c273426c8826aca780e826cc52
valeria caramellino <valeriacaramellino@gmail.com>
2023-08-26 19:04:49 +0200

    add style color in dashboard card

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container-fluid mt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
+    <div class="row justify-content-center ">
+        <div class="col-md-8 ">
+            <div class="card bg_gradiant text-light">
+                <div class="card-header ">
                     <h1>REGISTRAZIONE AVVENUTA CON SUCCESSO!</h1>
                 </div>
 
@@ -16,9 +16,9 @@
                     </div>
                     @endif
 
-                    <h4>BENVENUTO, GRAZIE PER ESERTI RESGISTRATO NEL NOSTRO SITO.</h4>
-                    <p>ti trovi nella tua pagina personale per monitorare e tenere sotto controllo tutto ciò che riguarda il tuo profilo alla tua sinistra avrai la possibilità di apportare alcune modifiche metti quante più nozioni possibili per avere la possibilità di farti notare il più possibile inoltre avrai anche la possibilità di essere sponsorizzato e far si che le tue visualizzazioni aumentino... <br>
-                    MA ORA DAI SUBITO UN'OCCHIO AL TUO PROFILO E APPORTA ALCUNE MODIFICHE!!
+                    <h4>BENVENUTO! GRAZIE PER ESSERTI REGISTRATO NEL NOSTRO SITO.</h4>
+                    <br>
+                   DAI SUBITO UN OCCHIO AL TUO PROFILO E APPORTA ALCUNE MODIFICHE!!
                     </p>
                 </div>
             </div>

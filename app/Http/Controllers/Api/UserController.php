410983e29c529d7ab82a1a6fb1858871a47841c0
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-08-30 12:04:58 +0200

    add api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+//import model USER
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+    public function index(){
+
+        $Users = User::all();
+
+        // $posts = Post::with("category", "tags")->get();
+
+        // $Users = User::paginate(5);
+
+        // $posts = User::with("category", "tags")->paginate(3);
+
+        $response = [
+            "success" => true,
+            "results" => $Users
+        ];
+
+        return response()->json($response);
+
+    }
+
+    // public function count(){
+
+    // }
+
+    // public function show() {
+
+    // }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//sanctum, funzionalita per gestire in modo easy l' autenticazione
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get("/apiDeveloper", function() {
+//     return response()->json();
+//     return"ok";
+// });
+//Chiamata al controller per la gestione dei dati API
+Route::get("/apiDeveloper", [UserController::class, "index"]);

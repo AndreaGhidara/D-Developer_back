046f522bd45b7babb577bd0624a3c67ae52b3b51
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-09-01 10:02:07 +0100

    edit user profile style

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -6,7 +6,7 @@
     <div class="row g-4 align-items-center justify-content-between">
         <div class="col-8">
             <a class="btn bg_lightgreen" href="{{ route("admin.users.edit", $user) }}">
-                <i class="fa fa-pencil" aria-hidden="true"></i> Modifica il tuo profilo
+                <i class="fa fa-pencil" aria-hidden="true"></i> Modifica profilo
             </a>
         </div>
 
@@ -15,32 +15,63 @@
     <div class="row g-4">
         <div class="col">
             <div>
-                {{-- Dati anagrafici developer --}}
-                <div class="card p-2 text-center px-5">
-                    <h1>{{$user->name}} {{$user->surname}}</h1>
-                    <p>{{ $user->email}}</p>
-                    <p>{{ $user->date_of_birth}}</p>
-                    <p>{{ $user->address}}</p>
-
-                    {{-- Immagine background --}}
-                    <div class="col-12 relative">
-                        @if ($user->bg_dev)
-                        <img src="{{asset("storage/".$user->bg_dev)}}" class="img-fluid border border-success border-5">
+                {{-- Immagine background --}}
+                <div class="col-12 relative">
+                    @if ($user->bg_dev)
+                    <img src="{{asset("storage/".$user->bg_dev)}}" class="img-fluid border border-success border-5">
+                    @else
+                    <img src="https://picsum.photos/1200/600?random" class="img-fluid border border-success border-5">
+                    @endif
+                    {{-- Immagine profilo --}}
+                    <div class="absolute">
+                        @if ($user->img_path)
+                        <img src="{{asset("storage/".$user->img_path)}}" class="img-fluid rounded-circle border border-success border-5">
                         @else
-                        <img src="https://picsum.photos/1200/600?random" class="img-fluid border border-success border-5">
+                        <img src="https://picsum.photos/300/300?random" class="img-fluid rounded-circle border border-success border-5">
                         @endif
-
-                        {{-- Immagine profilo --}}
-                        <div class="absolute">
-                            @if ($user->img_path)
-                            <img src="{{asset("storage/".$user->img_path)}}" class="img-fluid rounded-circle border border-success border-5">
-                            @else
-                            <img src="https://picsum.photos/300/300?random" class="img-fluid rounded-circle border border-success border-5">
-                            @endif
-                        </div>
                     </div>
+                </div>
+                {{-- Dati developer --}}
+                <div class="card p-2 text-center px-5">
+                    <div>
+                       <h1>{{$user->name}} {{$user->surname}}</h1>
+                    </div>
+                    <div>
+                        <p>{{ $user->email}}</p>
+                        <p>{{ $user->date_of_birth}}</p>
+                        <p>{{ $user->address}}</p>
+                        <p>Tel. {{ $user->phone_number}}</p>
+                        <p>{{ $user->vat_number}}</p>
+                    </div>
+                     {{-- Linguaggi di programmazione selezionati dall'utente --}}
+                    @if($user->programmingLanguages)
+                        <ul class="list-group border-0">
+                            <h3>I tuoi Linguaggi</h3>
+                            @foreach ($user->programmingLanguages as $language)
+                            <li  class="list-group-item border-0">{{$language->language}}</li>
+                            @endforeach
+                        </ul>
+                    @endif
 
-                    {{-- Link collegamenti social --}}
+                     {{-- Bio e Soft Skills --}}
+                     <h3>La tua Bio</h3>
+                     <p>{{ $user->bio}}</p>
+                     <h3>Le tue Soft Skills</h3>
+                     <p>{{ $user->soft_skill}}</p>
+
+                     @if(empty($user->sponsors))
+                         <ul class="list-group border-0">
+                             <h3>Sponsor</h3>
+                             @foreach ($user->sponsors as $sponsor)
+                             <li  class="list-group-item border-0">{{$sponsor->name}}</li>
+                             @endforeach
+                         </ul>
+                     @endif
+
+                    {{-- GitHub Linkedin e CV --}}
+                    <div class="row">
+                        <a href="{{asset('storage/' . $user->cv)}}" download="cv">Scarica il mio CV</a>
+                    </div>
                     <div class="row">
                         <div class="col-12 py-5 d-flex justify-content-evenly">
                             <a href="{{ $user->github_link}}" target="_blank"><i class="fa-brands fa-github"></i></a>
@@ -48,29 +79,7 @@
                         </div>
                     </div>
 
-                    {{-- Informazioni generali + skills --}}
-                    <p>{{ $user->bio}}</p>
-                    <p>{{ $user->vat_number}}</p>
-                    <a href="{{asset('storage/' . $user->cv)}}" download="cv">Scarica il mio CV</a>
-                    <p>Tel. {{ $user->phone_number}}</p>
-                    <p>{{ $user->soft_skill}}</p>
-                    @if($user->programmingLanguages)
-                        <ul class="list-group border-0">
-                            <h3>Linguaggi</h3>
-                            @foreach ($user->programmingLanguages as $language)
-                            <li  class="list-group-item border-0">{{$language->language}}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    @if(empty($user->sponsors))
-                        <ul class="list-group border-0">
-                            <h3>Sponsor</h3>
-                            @foreach ($user->sponsors as $sponsor)
-                            <li  class="list-group-item border-0">{{$sponsor->name}}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    <!--DELETE ACCOUNT BUTTON-->
+                    {{--EDIT E DELETE ACCOUNT BUTTON--}}
                     <div class="col-3 d-flex justify-content-end">
                         <form action="{{ route('admin.users.destroy', $user) }}" method="post">
                             @csrf

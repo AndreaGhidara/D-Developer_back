2f2ec761ed48b098d29cafee5d4b51c27cd6f871
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-09-04 15:20:02 +0200

    home redirect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron pb-4 py-5 bg-light bg_gradiant">
+<div class="walpaperHome d-flex align-items-center justify-content-center gradient-background" >
+    <div class="container">
+        <div class="row ">
+            <div class="col-8">
+                <h1 class="display-5 fw-bold">
+                    Benvenuto nel poratale dei <br>
+                    <span class="text-warning">
+                        Developer
+                    </span>
+                </h1>
+            </div>
+            <div class="col-4 p-0">
+                <img class="w-100 h-100 object-fit-cover" src="prova.png" alt="">
+            </div>
+            <div class="col-11 d-flex justify-content-between">
+                <a href="{{ route('register') }}">
+                    <button class="btn btn-primary text-white">
+                        Accedi/Registrati
+                    </button>
+                </a>
+                <button class="btn btn-secondary">
+                    <a class="text-white" href="{{ url('/homepage') }}">
+                        Visitatore
+                    </a>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- <div class="jumbotron pb-4 py-5 bg-light bg_gradiant">
     <div class="container">
         <div class="row text-center justify-content-center">
             <div class="logoBig">
@@ -24,5 +54,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection

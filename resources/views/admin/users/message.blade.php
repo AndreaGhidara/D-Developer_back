0603e04d021b91e357137f02b8cf10401f6c7215
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-09-06 12:23:03 +0100

    edit message and reviews style

--- resources/views/admin/users/message.blade.php
+++ resources/views/admin/users/message.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 <div class="container my-3 gradient-background-blue rounded-3 p-2">
     <div class="row text-center text-white m-3">
+        {{-- Titolo pagina --}}
         <div class="col">
-            <h1 class="text-white orangeBg rounded-5 p-2">Posta in arrivo</h1>
+            <h1 class="text-white orangeBg rounded-5 p-2">
+                Posta in arrivo
+            </h1>
         </div>
     </div>
     <div class="row bg-light rounded-5 m-5">
+        {{-- Inizio del ciclo --}}
         <div class="col">
             @foreach ($messages as $message)
-                <h4 class="rounded-3 text-center text-white orangeBg w-50 p-2 m-5">
+                <h4 class="rounded-3 text-center text-white mandarinBg w-50 p-2 m-5">
                     <i class="fa-solid fa-envelope-open-text"></i>
-                    Hai ricevuto un nuovo messaggio da :
+                    Hai ricevuto un messaggio da :
                 </h4>
+                {{-- Info utente che ha mandato il messaggio --}}
                 <div class="row text-center rounded-3 text-white orangeBg p-2 m-5">
                     <div class="col-md-6 d-flex">
                         <h4><i class="fa-regular fa-user"></i> {{$message->name}} {{$message->surname}}</h4>
@@ -22,13 +27,15 @@
                         <h5><i class="fa-regular fa-envelope"></i> {{$message->email}}</h5>
                     </div>
                 </div>
+                {{-- Messaggio ricevuto --}}
                 <div class="row rounded-3 text-white orangeBg p-2 m-5">
                     <div class="col">
                         <p>{{$message->text}}</p>
                     </div>
                 </div>
-                <hr>
+                <hr class="mandarinText">
             @endforeach
+            {{-- Fine del ciclo --}}
         </div>
     </div>
 </div>
--- resources/views/admin/users/reviews.blade.php
+++ resources/views/admin/users/reviews.blade.php
@@ -1,23 +1,42 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container my-3">
-    <div class="row g-4">
-        <div class="col">
-            <div>
-                <h3>Recensioni</h3>
-                <br>
-                @foreach ($reviews as $review)
-                <div class="list-group border border-0">
-                    <h4>{{$review->date}}</h4>
-                    <h4>{{$review->name}}</h4>
-                    <h5>{{$review->email}}</h5>
-                    <p>{{$review->review}}</p>
-                    <br>
-                @endforeach
-            </div>
+<div class="container my-3 gradient-background-blue rounded-3 p-2">
+    <div class="row">
+        <div class="col text-center p-3">
+            <h1 class="text-white orangeBg rounded-5 p-2">
+                Recensioni ricevute
+            </h1>
         </div>
     </div>
+    @foreach ($reviews as $review)
+        <div class="row d-flex justify-content-center text-center m-3 mb-3 rounded-3 bg-light">
+            <div class="col-sm-4 p-3">
+                <p class="text-white orangeBg rounded-3">
+                  <i class="fa-regular fa-user"></i>
+                </p>
+
+                <h4>{{$review->name}}</h4>
+            </div>
+            <div class="col-sm-3 p-3">
+                <p class="text-white orangeBg rounded-3">
+                    <i class="fa-regular fa-envelope"></i>
+                    Email
+                </p>
+                <h5>{{$review->email}}</h5>
+            </div>
+            <div class="col-sm-3 p-3">
+                <p class="text-white orangeBg rounded-3">Ricevuta in data:</p>
+                <h4>{{$review->date}}</h4>
+            </div>
+        </div>
+        <div class="row m-3 rounded-3 bg-light">
+            <div class="col pt-3">
+                <p>{{$review->review}}</p>
+            </div>
+        </div>
+        <hr class="mandarinText">
+    @endforeach
 </div>
 
 <script>

63ab9f51fe5b4999f1c52cdeebf402b45c425826
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-09-08 17:59:27 +0100

    edit mess style

--- resources/views/admin/users/message.blade.php
+++ resources/views/admin/users/message.blade.php
@@ -1,42 +1,45 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container my-3 gradient-background-blue rounded-3 p-2">
-    <div class="row text-center text-white m-3">
+<div class="container d-flex justify-content-center my-3 rounded-3 p-2">
+    <div class="row d-flex text-center text-white m-3">
         {{-- Titolo pagina --}}
         <div class="col">
-            <h1 class="text-white orangeBg rounded-5 p-2">
+            <h1 class="text-white mandarinBg rounded-5 p-3 w-100">
                 Posta in arrivo
             </h1>
         </div>
     </div>
-    <div class="row bg-light rounded-5 m-5">
+</div>
+<div class="container my-3 gradient-background-blue rounded-3 p-2 overflow-auto h-75">
+    <div class="row rounded-5 m-5">
         {{-- Inizio del ciclo --}}
-        <div class="col">
-            @foreach ($messages as $message)
-                <h4 class="rounded-3 text-center text-white mandarinBg w-50 p-2 m-5">
-                    <i class="fa-solid fa-envelope-open-text"></i>
-                    Hai ricevuto un messaggio da :
-                </h4>
+        @foreach ($messages as $message)
+        <div class="col-12 bg-light p-2 mb-5 rounded-5">
+            <div class="row p-5">
+                <div class="col-sm-7">
+                    <h4 class="rounded-3 text-center text-white mandarinBg titleW p-2">
+                        <i class="fa-solid fa-envelope-open-text"></i>
+                        Hai ricevuto un messaggio da :
+                    </h4>
+                </div>
                 {{-- Info utente che ha mandato il messaggio --}}
-                <div class="row text-center rounded-3 text-white orangeBg p-2 m-5">
-                    <div class="col-md-6 d-flex">
-                        <h4><i class="fa-regular fa-user"></i> {{$message->name}} {{$message->surname}}</h4>
-                    </div>
-                    <div class="col-md-6">
-                        <h5><i class="fa-regular fa-envelope"></i> {{$message->email}}</h5>
-                    </div>
+                <div class="col-sm-2 text-center rounded-3 text-white orangeBg m-2 p-2">
+                    <h4>{{$message->name}} {{$message->surname}}</h4>
                 </div>
-                {{-- Messaggio ricevuto --}}
-                <div class="row rounded-3 text-white orangeBg p-2 m-5">
-                    <div class="col">
-                        <p>{{$message->text}}</p>
-                    </div>
+                <div class="col-sm-2 text-center rounded-3 text-white orangeBg m-2 p-2">
+                    <h5><i class="fa-regular fa-envelope"></i> {{$message->email}}</h5>
                 </div>
-                <hr class="mandarinText">
-            @endforeach
-            {{-- Fine del ciclo --}}
+            </div>
+            <div class="row rounded-3 text-white orangeBg p-2 m-5">
+                <div class="col">
+                    <p>{{$message->text}}</p>
+                </div>
+            </div>
+            <hr class="mandarinText">
         </div>
+        @endforeach
+        {{-- Fine del ciclo --}}
     </div>
 </div>
 

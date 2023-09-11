6baf8927872cfdf2ba458479a36e1cf9611f868f
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-09-11 17:58:40 +0200

    dati reali

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->text('bg_dev')->nullable();
             $table->text('github_link')->nullable();
             $table->text('linkedin_link')->nullable();
-            $table->string('vat_number')->unique()->nullable();
+            $table->string('vat_number')->nullable();
             $table->text('cv')->nullable();
             $table->string('phone_number')->nullable();
             $table->text('soft_skill')->nullable();
